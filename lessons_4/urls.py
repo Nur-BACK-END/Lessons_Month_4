ce8f9b014ffd91acf06c671a4a6a23bfdeb1a6a3
Nur-BACK-END <nurelarynov46@gmail.com>
2025-01-14 23:55:40 +0600

    HW3:добавление тега и категории

--- lessons_4/urls.py
+++ lessons_4/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from posts.views import main_view, list_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-urlpatterns = [
+urlpatterns = (
+    [
     path('admin/', admin.site.urls),
     path('' , main_view),
     path('posts/', list_view),
-]
+    ]
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
-class Post(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return f'Category: {self.title}'
+    
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Teg: {self.title}"
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/',null=True,blank=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400,blank=True)
     rate = models.IntegerField(default=0)
     content = models.TextField(max_length=1000, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name="posts",null=True,blank=True)
+    tag = models.ManyToManyField('Tag', related_name="posts", blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.description}"
+    
